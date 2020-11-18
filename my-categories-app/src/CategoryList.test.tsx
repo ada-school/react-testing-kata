e86e183781698c1911248da0dd858a010b356a66
Gabriel Bernal <gabriel.bernal@holidaycheck.com>
2020-11-18 14:34:49 +0100

    add list interaction

--- my-categories-app/src/CategoryList.test.tsx
+++ my-categories-app/src/CategoryList.test.tsx
@@ -1,9 +1,10 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import { CategoryList } from "./CategoryList";
+import { Category } from "./models";
 
 test("Se renderiza una lista de categorias", () => {
-  const categories = [
+  const categories: Array<Category> = [
     {
       id: 1,
       name: "Categoria 1",
@@ -21,13 +22,40 @@ test("Se renderiza una lista de categorias", () => {
   expect(renderResult.container).toMatchSnapshot();
 
   categories.forEach((category) =>
-    expect(screen.getByText(category.name).tagName).toEqual("li")
+    expect(screen.getByText(category.name).tagName).toEqual("LI")
+  );
+});
+
+// hacer click
+// mostrar las subcategorias
+// mostrar las seleccionada
+
+test("Cuando se hace click en la categoria, se llama a la funcion onCategoryClick", () => {
+  const mockOnCategoryClick = jest.fn();
+  const categories: Array<Category> = [
+    {
+      id: 1,
+      name: "Categoria 1",
+    },
+    {
+      id: 2,
+      name: "Categoria 2",
+    },
+    {
+      id: 3,
+      name: "Categoria 3",
+    },
+  ];
+
+  render(
+    <CategoryList
+      categories={categories}
+      onCategoryClick={mockOnCategoryClick}
+    />
   );
 
-  /*
-<ul>
-    <li>sd</li
-    <li>sd</li
-</ul>
-*/
+  const chosenCategory = screen.getByText(categories[0].name);
+  fireEvent.click(chosenCategory);
+
+  expect(mockOnCategoryClick).toHaveBeenCalled();
 });
--- my-categories-app/src/CategoryList.tsx
+++ my-categories-app/src/CategoryList.tsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { Category } from "./models";
 
 interface CategoryListProps {
-  categories: ReadonlyArray<any>;
+  categories: ReadonlyArray<Category>;
+  onCategoryClick?: (category: Category) => void;
 }
 
-export const CategoryList: React.FC<CategoryListProps> = () => {
-  return <div style={{ color: "red" }} />;
+// function currying
+
+// const a = (b:string) => { return (c:string)=> b + c}
+
+// const f = (a:string, b:string) => a+b;
+
+// const toReuse = a("santiago")
+// const result = toReuse("carrillo") // santiago carrillo
+
+// const result3 = toReuse("barbosa") // santiago barbosa
+
+// const result2 = f("santiago","carrillo")
+
+export const CategoryList: React.FC<CategoryListProps> = ({
+  categories,
+  onCategoryClick,
+}) => {
+  const handleCategoryClick = (category: Category) => (e: React.MouseEvent) => {
+    if (onCategoryClick) {
+      onCategoryClick(category);
+    }
+  };
+
+  const categoryItems = categories.map((category) => (
+    <li key={category.id} onClick={handleCategoryClick(category)}>
+      {category.name}
+    </li>
+  ));
+  return <ul>{categoryItems}</ul>;
 };
